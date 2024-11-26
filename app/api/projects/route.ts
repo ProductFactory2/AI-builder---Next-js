54af3c8bdb6cf334d5e4afef86a6a2f8412d5de7
venkat <venkat.a.ihub@snsgroups.com>
2024-11-26 10:09:50 +0530

    adding preview page with ui

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -24,11 +24,7 @@ export const GET = async(request: Request) => {
 export async function POST(request: Request) {
   try {
     await connectMongoDB();
-<<<<<<< HEAD
     const { name, technologies, userId, finalPrompt, referenceFile } = await request.json();
-=======
-    const { name, technologies, userId, finalPrompt } = await request.json();
->>>>>>> origin/preview_navigation_complete
     
     if (!userId) {
       return new Response('Unauthorized', { status: 401 });
@@ -38,12 +34,8 @@ export async function POST(request: Request) {
       name,
       technologies,
       userId,
-<<<<<<< HEAD
       finalPrompt,
       referenceFile
-=======
-      finalPrompt
->>>>>>> origin/preview_navigation_complete
     });
     
     return new Response(JSON.stringify(project), {
